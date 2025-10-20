60d995c559937f0a7e4a603f2f1df37528a0a1b1
aayaancoder1 <aayaan.shahwaiz@gmail.com>
2025-10-20 16:07:08 +0530

    forgot flowlayout, wrote actionListener instead of actionEvent and forgot to mention this in assActionListener()

---
+++ Rounds/OddPairs.java
@@ -0,0 +1,20 @@
+/* Given a positive integer n, print all pairs (i, j) such that
+
+1 <= i < j <= n,
+
+and the sum i + j is odd.
+
+Then print the total number of such pairs at the end.
+
+Example:
+
+Input: 5
+Output:
+(1,2)
+(1,4)
+(2,3)
+(2,5)
+(3,4)
+(4,5)
+Total pairs: 6
+*/
